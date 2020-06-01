c7407cef5002963324c1a2a678d7201ca541a5b6
Benoit Caron <bencaron@gmail.com>
2020-05-31 21:59:30 -0400

    Adding missing informations

--- collector.go
+++ collector.go
@@ -67,7 +67,7 @@ type solarCollector struct {
 	batteryMaxVoltage *prometheus.Desc
 	batteryMinVoltage *prometheus.Desc
 
-	DeviceTemp *prometheus.Desc
+	deviceTemp *prometheus.Desc
 
 	loadActive  *prometheus.Desc
 	loadVoltage *prometheus.Desc
@@ -129,7 +129,102 @@ func newSolarCollector() *solarCollector {
 		),
 		batteryTemp: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "", "battery_temp"),
-			"Battery temperature (controller sensor) (Celcius).",
+			"Battery temperature (external sensor) (Celcius).",
+			nil, // no labels yet
+			nil,
+		),
+		batteryMaxVoltage: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "battery_max_voltage"),
+			"Maximum battery voltage (V).",
+			nil, // no labels yet
+			nil,
+		),
+		batteryMinVoltage: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "battery_min_voltage"),
+			"Minimum battery voltage (V).",
+			nil, // no labels yet
+			nil,
+		),
+
+		deviceTemp: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "device_temp"),
+			"Device temperature (controller sensor) (Celcius).",
+			nil, // no labels yet
+			nil,
+		),
+
+		loadActive: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "load_active"),
+			"Load output is active (bool)",
+			nil, // no labels yet
+			nil,
+		),
+		loadVoltage: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "load_voltage"),
+			"Load voltage (V).",
+			nil, // no labels yet
+			nil,
+		),
+		loadCurrent: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "load_current"),
+			"Load current (A).",
+			nil, // no labels yet
+			nil,
+		),
+		loadPower: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "load_power"),
+			"Load power (W).",
+			nil, // no labels yet
+			nil,
+		),
+
+		energyConsumedDaily: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_consumed_daily"),
+			"Controller calculated daily consumption, (kWh)",
+			nil, // no labels yet
+			nil,
+		),
+
+		energyConsumedMonthly: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_consumed_monthly"),
+			"Controller calculated monthly consumption, (kWh)",
+			nil, // no labels yet
+			nil,
+		),
+		energyConsumedAnnual: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_consumed_annual"),
+			"Controller calculated annual consumption, (kWh)",
+			nil, // no labels yet
+			nil,
+		),
+		energyConsumedTotal: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_consumed_taotal"),
+			"Controller calculated total consumption, (kWh)",
+			nil, // no labels yet
+			nil,
+		),
+
+		energyGeneratedDaily: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_generated_daily"),
+			"Controller calculated daily power generation, (kWh)",
+			nil, // no labels yet
+			nil,
+		),
+		energyGeneratedMonthly: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_generated_monthly"),
+			"Controller calculated monthly power generation, (kWh)",
+			nil, // no labels yet
+			nil,
+		),
+		energyGeneratedAnnual: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_generated_annual"),
+			"Controller calculated annual power generation, (kWh)",
+			nil, // no labels yet
+			nil,
+		),
+		energyGeneratedTotal: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "energy_generated_total"),
+			"Controller calculated total power generation, (kWh)",
 			nil, // no labels yet
 			nil,
 		),
@@ -172,12 +267,14 @@ func (c *solarCollector) collect(ch chan<- prometheus.Metric) error {
 	if err != nil {
 		return err
 	}
+
 	/*
-		// fmt.Println(tracer)
-		panelVoltage.Set(float64(tracer.ArrayVoltage))
-		panelCurrent.Set(float64(tracer.ArrayCurrent))
-		panelPower.Set(float64(tracer.ArrayPower))
-	*/
+	 *  report the collected data
+	 */
+
+	// store boolean values as a float (1 == true, 0 == false)
+	var loadIsActive float64
+	// Panel array
 	ch <- prometheus.MustNewConstMetric(
 		c.panelVoltage,
 		prometheus.CounterValue,
@@ -188,13 +285,23 @@ func (c *solarCollector) collect(ch chan<- prometheus.Metric) error {
 		prometheus.CounterValue,
 		float64(tracer.ArrayCurrent),
 	)
-	// Bloc pou les batteries
+	ch <- prometheus.MustNewConstMetric(
+		c.panelPower,
+		prometheus.CounterValue,
+		float64(tracer.ArrayPower),
+	)
+
+	// Batteries
 	ch <- prometheus.MustNewConstMetric(
 		c.batteryCurrent,
 		prometheus.CounterValue,
 		float64(tracer.BatteryCurrent),
 	)
-
+	ch <- prometheus.MustNewConstMetric(
+		c.batteryVoltage,
+		prometheus.CounterValue,
+		float64(tracer.BatteryVoltage),
+	)
 	ch <- prometheus.MustNewConstMetric(
 		c.batterySOC,
 		prometheus.CounterValue,
@@ -205,6 +312,92 @@ func (c *solarCollector) collect(ch chan<- prometheus.Metric) error {
 		prometheus.CounterValue,
 		float64(tracer.BatteryTemp),
 	)
+	ch <- prometheus.MustNewConstMetric(
+		c.batteryMinVoltage,
+		prometheus.CounterValue,
+		float64(tracer.BatteryMinVoltage),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.batteryMaxVoltage,
+		prometheus.CounterValue,
+		float64(tracer.BatteryMaxVoltage),
+	)
+
+	// Load output
+	if tracer.Load {
+		loadIsActive = 1
+	}
+	ch <- prometheus.MustNewConstMetric(
+		c.loadActive,
+		prometheus.CounterValue,
+		loadIsActive,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.loadVoltage,
+		prometheus.CounterValue,
+		float64(tracer.LoadVoltage),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.loadCurrent,
+		prometheus.CounterValue,
+		float64(tracer.LoadCurrent),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.loadPower,
+		prometheus.CounterValue,
+		float64(tracer.LoadPower),
+	)
+
+	// controller infos
+	ch <- prometheus.MustNewConstMetric(
+		c.deviceTemp,
+		prometheus.CounterValue,
+		float64(tracer.DeviceTemp),
+	)
+
+	// energy consumed
+	ch <- prometheus.MustNewConstMetric(
+		c.energyConsumedDaily,
+		prometheus.CounterValue,
+		float64(tracer.EnergyConsumedDaily),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.energyConsumedMonthly,
+		prometheus.CounterValue,
+		float64(tracer.EnergyConsumedMonthly),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.energyConsumedAnnual,
+		prometheus.CounterValue,
+		float64(tracer.EnergyConsumedAnnual),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.energyConsumedTotal,
+		prometheus.CounterValue,
+		float64(tracer.EnergyConsumedTotal),
+	)
+	// energy generated
+	ch <- prometheus.MustNewConstMetric(
+		c.energyGeneratedDaily,
+		prometheus.CounterValue,
+		float64(tracer.EnergyGeneratedDaily),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.energyGeneratedMonthly,
+		prometheus.CounterValue,
+		float64(tracer.EnergyGeneratedMonthly),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.energyGeneratedAnnual,
+		prometheus.CounterValue,
+		float64(tracer.EnergyGeneratedAnnual),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.energyGeneratedTotal,
+		prometheus.CounterValue,
+		float64(tracer.EnergyGeneratedTotal),
+	)
+
 	return nil
 }
 
